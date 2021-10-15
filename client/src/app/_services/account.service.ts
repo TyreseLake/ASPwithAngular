94c24c9b211fc665a9e869472f9fb5f78e201bc4
TyreseLake <tlfoot@hotmail.com>
2021-10-15 15:56:37 -0400

    Registration

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -16,12 +16,25 @@ export class AccountService {
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map((response: User) => {
+      map((response) => {
         const user = response;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
+        return user;
+      })
+    )
+  }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
+      map(user => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
       })
     )
   }

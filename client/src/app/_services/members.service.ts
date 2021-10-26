95e3ff683649fd7df9b845d741a9173eab38eff0
TyreseLake <tlfoot@hotmail.com>
2021-10-26 15:22:50 -0400

    Updated Interface

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,13 +1,15 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')!)?.token
-  })
-}
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')!)?.token
+//   })
+// }
 
 
 @Injectable({
@@ -15,14 +17,32 @@ const httpOptions = {
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
   
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    if(this.members.length > 0) return of(this.members)
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+    const member = this.members.find(x => x.userName === username);
+    if(member != undefined) return of(member)
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
